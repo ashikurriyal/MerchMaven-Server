98cf883d51d25ab3c3a753f7ab5215c4a538c3e8
ashikurriyal <riyal.ashikur@gmail.com>
2024-08-20 19:35:12 +0600

    Updated the API

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // const cookieParser = require('cookie-parser');
-const app = express()
-require('dotenv').config()
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5300;
 
-
-app.use(express.json())
-app.use(cors({
+app.use(express.json());
+app.use(
+  cors({
     origin: [
-        'http://localhost:5173',
-        'https://merch-maven-server.vercel.app/'
-    ]
-}));
+      "http://localhost:5173",
+      "https://merch-maven-server.vercel.app",
+      "https://merch-maven.web.app",
+    ],
+  })
+);
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbm5u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbm5u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +25,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,7 +34,47 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const productsCollection = client
+      .db("MerchMavenDB")
+      .collection("productsCollection");
+
+    app.get("/products", async (req, res) => {
+      const page = parseInt(req.query.page) || 0;
+      const size = parseInt(req.query.size) || 9;
+      const search = req.query.search || "";
+      const sortType = req.query.sortType || "price";
+      const sortOrder = req.query.sortOrder === "desc" ? -1 : 1;
+      
+
+      const query = {
+        ...search && { productName: { $regex: search, $options: "i" } }
+      };
+
+      const sort = { [sortType]: sortOrder };
+
+      const result = await productsCollection
+        .find(query)
+        .sort(sort)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/productsCount", async (req, res) => {
+      const search = req.query.search || "";
+
+      // Define the search query for counting
+      const query = search
+        ? { productName: { $regex: search, $options: "i" } }
+        : {};
+      const count = await productsCollection.estimatedDocumentCount(query);
+      res.send({ count });
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -41,9 +82,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Merch Maven Server is OnGoing!')
-})
+app.get("/", (req, res) => {
+  res.send("Merch Maven Server is OnGoing!");
+});
 app.listen(port, () => {
-    console.log(`Server is running on port${port}`)
-})
+  console.log(`Server is running on port${port}`);
+});
